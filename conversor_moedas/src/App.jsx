a04fb364d9e6cac2a3646a2a4451ccae854d8dab
mraduarte <mcduarte9@hotmail.com>
2025-03-24 21:56:31 -0300

    Desenvolvimento do projeto de Conversor de moedas utilizando API

---
+++ conversor_moedas/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CurrencyConverter from './assets/components/CurrencyConverter'
+
+function App() {
+
+  return (
+    <>
+    <CurrencyConverter/>
+    </>
+  )
+}
+
+export default App
